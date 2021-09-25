74785977dcc4854500088cfaa47139e06803e328
MarcosFitzsimons <marcosfitzsimons@hotmail.com>
2021-09-25 20:45:20 -0300

    add-new-header-and-improvements

--- js/scripts2.js
+++ js/scripts2.js
@@ -1,11 +1,23 @@
+/*==================== Nav Toggle ====================*/
+
+const navToggle = document.querySelector('.hamburger')
+const nav = document.querySelector('.nav')
+const navLinks = document.querySelectorAll('.nav__enlace')
+
+navToggle.addEventListener('click', () => {
+    navToggle.classList.toggle('active');
+    nav.classList.toggle('active')
+})
+
 /*==================== Nav Sticky Home Page ====================*/
 
 window.addEventListener("scroll", function(){
     let header = document.querySelector("header")
     header.classList.toggle("sticky", window.scrollY > 500)
+    header.classList.toggle("center", window.scrollY > 500)
     header.classList.toggle("border-bottom", window.scrollY > 500)
+    navToggle.classList.toggle("display-none", window.scrollY > 500)
 })
-
 
 /*==================== TESTIMONIAL ====================*/
 
